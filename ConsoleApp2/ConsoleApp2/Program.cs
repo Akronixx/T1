8daae55a64d361bdcd057427a308c9488dec74a3
Akronixx <josectp12yt@gmail.com>
2024-09-19 09:17:31 -0500

    Final P2

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -22,7 +22,7 @@ relación:
 El programa funcionará hasta que se digite F, mostrando cuantos entraron por cada color
 Hará una prueba con estos datos: 2, 3, 4, 6, 9, F*/
             string n;
-            double placa;
+            double placa,verde=0,rojo=0,azul=0;
             Console.WriteLine("[=============================CONTROL DE TRAFICO=============================]");
             Console.WriteLine("Desea saber la calcomanía del vehículo(Si(S)/Fin(F)): ");
             n= Console.ReadLine().ToUpper();
@@ -34,9 +34,15 @@ Hará una prueba con estos datos: 2, 3, 4, 6, 9, F*/
             }
             while (n == "S")
             {
-                Console.Write("Ingresé el ultimo digito de la placa del vehiculo(Si(S)/Fin(F)): ");
+                Console.Write("Ingresé el ultimo digito de la placa del vehiculo: ");
                 placa = int.Parse(Console.ReadLine());
-                Console.WriteLine("Desea saber la calcomanía del vehículo: ");
+                if (placa > 0 && placa <= 3)
+                    verde = verde + 1;
+                if (placa > 3 && placa <= 6)
+                    azul = azul + 1;
+                if (placa > 6 && placa <= 9)
+                    rojo = rojo + 1;
+                Console.WriteLine("Desea saber la calcomanía del vehículo(Si(S)/Fin(F)): ");
                 n = Console.ReadLine().ToUpper();
                 while (n != "S" && n != "F")
                 {
@@ -45,6 +51,10 @@ Hará una prueba con estos datos: 2, 3, 4, 6, 9, F*/
                     n = Console.ReadLine().ToUpper();
                 }
             }
+            Console.WriteLine("Número de vehiculos verdes: " + verde);
+            Console.WriteLine("Número de vehiculos azules: " + azul);
+            Console.WriteLine("Número de vehiculos rojos: " + rojo);
+            Console.ReadKey();
         }
     }
 }
